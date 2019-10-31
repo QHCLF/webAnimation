212ad2ef3f70a54c2da74f8b8a70e43888a8dfe3
qhclf <3056149117@qq.com>
2019-10-31 15:45:13 +0800

    add canvas animation

--- canvas动画/util/ball.js
+++ canvas动画/util/ball.js
@@ -1,6 +1,6 @@
-function Ball(radius, color){
-    if(radius === undefined){ radius = 40;}
-    if(color === undefined){color = "pink";}
+function Ball(radius,color){
+    if(radius === undefined) {radius = 40;}
+    if(color === undefined){color = '#00ff00';}
     this.x = 0;
     this.y = 0;
     this.vx = 0;
@@ -10,32 +10,34 @@ function Ball(radius, color){
     this.mass = 1;
     this.scaleX = 1;
     this.scaleY = 1;
-    this.name = '';
-    this.color = utils.parseColor(color);
+    this.name = "";
+    this.color = util.parseColor(color);
     this.lineWidth = 1;
+
 }
 
-Ball.prototype.draw = function(ctx){
-    ctx.save();
-    ctx.translat(this.x, this.y);
-    ctx.rotate(this.rotation);
-    ctx.scale(this.scaleX, this.scaleY);
-    ctx.lineWidth = this.lineWidth;
-    ctx.fillStyle = this.color;
-    ctx.strokeStyle = this.color;
-    ctx.beginPath();
-    ctx.arc(0, 0, this.radius, 0, Math.PI * 2, false);
-    ctx.closePath();
-    ctx.fill();
-    ctx.stroke();
-    ctx.restore();
+Ball.prototype.draw = function(context){
+    context.save();
+    context.translate(this.x,this.y);
+    context.rotate(this.rotation);
+    context.scale(this.scaleX,this.scaleY);
+    context.lineWidth = this.lineWidth;
+    context.fillStyle = this.color;
+    context.strokeStyle = this.color;
+    context.beginPath();
+    context.arc(0,0,this.radius,0,Math.PI*2,false);
+    context.closePath();
+    context.fill();
+    context.stroke();
+    context.restore();
 }
 
+//得到球体的左上角坐标
 Ball.prototype.getBounds = function(){
     return {
         x: this.x - this.radius,
         y: this.y - this.radius,
-        width: this.radius * 2,
-        height: this.radius * 2;
-    }
+        width: this.radius*2,
+        height: this.radius*2
+    };
 }
--- canvas动画/util/util.js
+++ canvas动画/util/util.js
@@ -72,6 +72,24 @@ window.util.captureTouch = function(el){//触摸事件
     return touch;
 }
 
+window.util.parseColor = function (color, toNumber) {
+    if (toNumber === true) {
+      if (typeof color === 'number') {
+        return (color | 0); //chop off decimal
+      }
+      if (typeof color === 'string' && color[0] === '#') {
+        color = color.slice(1);
+      }
+      return window.parseInt(color, 16);
+    } else {
+      if (typeof color === 'number') {
+        color = '#' + ('00000' + (color | 0).toString(16)).substr(-6); //pad
+      }
+      return color;
+    }
+  };
+  
+
 // 对requestAnimationFrame处理兼容
 
 if(!window.requestAnimationFrame){
